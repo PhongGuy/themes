83037f2d42ab5c65173c9209ef15c443d5a6fecd
Daniel Olesen <36205789+PhongGuy@users.noreply.github.com>
2020-12-03 23:43:17 +0100

    fixed build bindings

--- build.js
+++ build.js
@@ -2,13 +2,27 @@ var fs = require('fs');
 const dist = './dist'
 
 fs.readdir(dist, (e, files) => {
-    if (files) {
-        for (const file of files) {
-            if (!file.includes('user.css')) {
-                fs.rename(`${dist}/${file}`, `${dist}/${file.replace('css', 'user.css')}`, (err) => {
-                    console.log(err);
-                })
-            }
+    for (const file of files) {
+        const path = `${dist}/${file}`;
+        console.log("\x1b[36m", 'Checking ', path);
+
+        // check for comments that are bindings
+        const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+        if (data.includes('/*![[')) {
+            console.log("\x1b[33m", 'Fixing bindings...')
+            var replaced = data.replace('/*![[', '/*[[');
+            // after replacing sendt it back
+            fs.writeFileSync(path, replaced);
         }
+
+        // add user to make it better?
+        if (!file.includes('user.css')) {
+            console.log('\x1b[33m', 'Adding user...')
+            fs.renameSync(path, `${dist}/${file.replace('css', 'user.css')}`, (err) => {
+                console.log(err);
+            });
+        }
+
+        console.log("\x1b[32m", 'Success')
     }
 });
