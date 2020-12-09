8ad568ba267e4b1ec7be58c330f86c666fff3f3e
Daniel Olesen <36205789+PhongGuy@users.noreply.github.com>
2020-12-09 16:37:34 +0100

    fixed newline

--- new.js
+++ new.js
@@ -14,5 +14,5 @@ file.write('@version      1.0.0\n');
 file.write('@homepageURL  https://github.com/PhongGuy/css-styles\n');
 file.write(`@updateURL    https://raw.githubusercontent.com/PhongGuy/css-styles/main/dist/${name}.user.css\n`);
 file.write('==/UserStyle== */\n');
-file.write(`@-moz-document domain("${domain}") {\n$backgound: #121212!important;\n$text: rgb(170,170,170)!important;\n $text-highlight: rgb(255,255,255)!important;$popup-background: rgba(33,33,33,.98)!important;\n$popup-hover: rgba(255,255,255,.1)!important;\n}`);
+file.write(`@-moz-document domain("${domain}") {\n$backgound: #121212!important;\n$text: rgb(170,170,170)!important;\n$text-highlight: rgb(255,255,255)!important\n;$popup-background: rgba(33,33,33,.98)!important;\n$popup-hover: rgba(255,255,255,.1)!important;\n}`);
 file.close();
