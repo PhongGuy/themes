33510eb0cac1aa011c8d15c07a7c6ff8e65557d2
Daniel Olesen <36205789+PhongGuy@users.noreply.github.com>
2021-01-21 17:38:08 +0100

    cleanup new method

--- new.js
+++ new.js
@@ -6,6 +6,8 @@ const split = name.split('-')[1];
 const themeName = `${domain} ${split.charAt(0).toUpperCase() + split.slice(1)}`
 
 const file = fs.createWriteStream(`${themes}/${name}.scss`);
+const css = fs.readFileSync('./new.scss', { encoding: 'utf8', flag: 'r' });
+console.log(css)
 file.write('/*! ==UserStyle==\n');
 file.write(`@name         ${themeName}\n`);
 file.write('@namespace    PhongGuy\n');
@@ -14,5 +16,5 @@ file.write('@version      1.0.0\n');
 file.write('@homepageURL  https://github.com/PhongGuy/themes\n');
 file.write(`@updateURL    https://raw.githubusercontent.com/PhongGuy/themes/main/dist/${name}.user.css\n`);
 file.write('==/UserStyle== */\n');
-file.write(`@-moz-document domain("${domain}") {\n$backgound:#121212!important;\n$text:rgb(170,170,170)!important;\n$text-highlight:rgb(255,255,255)!important\n;$popup-background:rgba(33,33,33,.98)!important;\n$popup-hover:rgba(255,255,255,.1)!important;\n$none:none!important;\n$sidebar:#191919!important;\n}`);
+file.write(`@-moz-document domain("${domain}") {\n${css}\n}`);
 file.close();
