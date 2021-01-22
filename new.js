c481176257767fc625532bc2f82704d6465d1955
Daniel Olesen <36205789+PhongGuy@users.noreply.github.com>
2021-01-22 15:42:00 +0100

    cleanup the new command

--- new.js
+++ new.js
@@ -5,16 +5,11 @@ const domain = name.includes('-') ? name.split('-')[0] : name;
 const split = name.split('-')[1];
 const themeName = `${domain} ${split.charAt(0).toUpperCase() + split.slice(1)}`
 
+const newTheme = fs.readFileSync('./new.scss', { encoding: 'utf8', flag: 'r' })
+    .replace('[[DOMAIN]]', domain)
+    .replace('[[NAME]]', name)
+    .replace('[[THEME]]', themeName);
+
 const file = fs.createWriteStream(`${themes}/${name}.scss`);
-const css = fs.readFileSync('./new.scss', { encoding: 'utf8', flag: 'r' });
-console.log(css)
-file.write('/*! ==UserStyle==\n');
-file.write(`@name         ${themeName}\n`);
-file.write('@namespace    PhongGuy\n');
-file.write('@author       PhongGuy\n');
-file.write('@version      1.0.0\n');
-file.write('@homepageURL  https://github.com/PhongGuy/themes\n');
-file.write(`@updateURL    https://raw.githubusercontent.com/PhongGuy/themes/main/dist/${name}.user.css\n`);
-file.write('==/UserStyle== */\n');
-file.write(`@-moz-document domain("${domain}") {\n${css}\n}`);
+file.write(newTheme);
 file.close();
