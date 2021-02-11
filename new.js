56cf836f512ada2e81ec7d3d3b598a822f8baf9f
Daniel Olesen <36205789+PhongGuy@users.noreply.github.com>
2021-02-11 15:39:06 +0100

    added logging

--- new.js
+++ new.js
@@ -1,9 +1,13 @@
-var fs = require('fs');
+const fs = require('fs');
+const colors = require('colors');
 const themes = './src/themes';
+
 const name = process.argv[2];
 const domain = name.includes('-') ? name.split('-')[0] : name;
 const split = name.split('-')[1];
 const themeName = `${domain} ${split.charAt(0).toUpperCase() + split.slice(1)}`
+
+console.log(`Adding ${themeName}`.yellow);
 
 const newTheme = fs.readFileSync('./new.scss', { encoding: 'utf8', flag: 'r' })
     .replace('[[DOMAIN]]', domain)
